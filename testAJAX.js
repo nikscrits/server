c5805bee00acf04a7e2226c230e2783873c07b23
Nicola Critten <ucesncr@ucl.ac.uk>
2018-03-01 06:54:26 -0500

    Editing the AJAX.js file to change to 'test2.html' on button press

--- testAJAX.js
+++ testAJAX.js
@@ -1,7 +1,7 @@
 var  xhr;  // define the global variable to process the AJAX request 
 function callDivChange() {   
 	xhr = new XMLHttpRequest();   
-	xhr.open("GET", "test.html", true);   
+	xhr.open("GET", "test2.html", true);   
 	xhr.onreadystatechange = processDivChange;   
 	try {      
 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");   
