82209c2f8ef26607cd1ce7d53e15131e3000c4c1
Nicola Critten <ucesncr@ucl.ac.uk>
2018-03-01 09:24:41 -0500

    Editing the server to establish a connection to PostgreSQL/PostGIS.

--- server.js
+++ server.js
@@ -19,3 +19,36 @@ var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toS
 var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4443);
+
+// read in the file and force it to be a string by adding “” at the beginning 
+var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js"); 
+
+// now convert the configruation file into the correct format -i.e. a name/value 
+pair array 
+var configarray = configtext.split(","); 
+var config = {}; 
+
+for (var i = 0; i < configarray.length; i++) {     
+	var split = configarray[i].split(':'); 
+	config[split[0].trim()] = split[1].trim();
+}
+
+var pg = require('pg'); 
+var pool = new pg.Pool(config); 
+
+app.get('postgistest', function (req,res) { 
+pool.connect(function(err,client,done) {        
+	if(err){            
+		console.log("not able to get connection "+ err);            
+		res.status(400).send(err);        
+	}         
+	client.query('SELECT name FROM united_kingdom_counties',function(err,result) {            
+		done();             
+		if(err){                
+			console.log(err);                
+			res.status(400).send(err);            
+		}            
+		res.status(200).send(result.rows);        
+		});     
+	}); 
+});
