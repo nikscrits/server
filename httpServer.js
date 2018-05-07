de5939115fae28b0691d6e8cb6229016be6dc1d5
Nicola Critten <ucesncr@ucl.ac.uk>
2018-05-07 16:20:55 -0400

    Commenting and formatting.

--- httpServer.js
+++ httpServer.js
@@ -1,15 +1,13 @@
-// express is the server that forms part of the nodejs program
+//Express is the server that forms part of the nodejs program
 var express = require('express');
 var path = require("path");
 var app = express();
 var bodyParser = require('body-parser');
 
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-
 
 //Adding functionality to allow cross-domain queries when PhoneGap is running a server
 	app.use(function(req, res, next) {
@@ -19,162 +17,167 @@ app.use(bodyParser.json());
 		next();
 	});
 
-	
+//This function uploads the question set by the user to the database
 app.post('/uploadData',function(req,res){
-       // note that we are using POST here as we are uploading data
-       // so the parameters form part of the BODY of the request rather than the RESTful API
-       console.dir(req.body);
-       pool.connect(function(err,client,done) {
-             if(err){
-             console.log("not able to get connection "+ err);
-             res.status(400).send(err);
-             }
+  //Using POST here as we are uploading data
+  console.dir(req.body);
+  pool.connect(function(err,client,done) {
+    if(err){
+      console.log("not able to get connection "+ err);
+      res.status(400).send(err);
+    }
 
-            // pull the geometry component together
-            // note that well known text requires the points as longitude/latitude !
-            // well known text should look like: 'POINT(-71.064544 42.28787)'
-            var geometrystring = "st_geomfromtext('POINT(" + req.body.long + " " + req.body.lat + ")'";
+    //Pull the geometry (longitude/latitude) component together
+    var geometrystring = "st_geomfromtext('POINT(" + req.body.long + " " + req.body.lat + ")'";
 
-             var querystring = "INSERT into quizquestions (point_name,question,answer1,answer2,answer3,answer4,correct_answer,coordinates) values ('";
-             querystring = querystring + req.body.pointname + "','" + req.body.question + "','" + req.body.answer1+"','" + req.body.answer2+"','" + req.body.answer3+"','" + req.body.answer4+"','" + req.body.correctanswer+"'," + geometrystring +"))";
-             console.log(querystring);
-             client.query( querystring,function(err,result) {
-          done();
-          if(err){
-               console.log(err);
-               res.status(400).send(err);
-          }
-          res.status(200).send("Question sent to database!");
-       });
-}); });
+    //Creates the string with the SQL insert statement
+    var querystring = "INSERT into quizquestions (point_name,question,answer1,answer2,answer3,answer4,correct_answer,coordinates) values ('";
+    querystring = querystring + req.body.pointname + "','" + req.body.question + "','" + req.body.answer1+"','" + req.body.answer2+"','" + req.body.answer3+"','" + req.body.answer4+"','" + req.body.correctanswer+"'," + geometrystring +"))";
+    console.log(querystring);
+    
+    client.query( querystring,function(err,result) {
+      done();
+      
+      if(err){
+        console.log(err);
+        res.status(400).send(err);
+      }
+      
+      res.status(200).send("Question sent to database!");
+    });
+  }); 
+});
 
 
-	// adding functionality to log the requests
-	app.use(function (req, res, next) {
-		var filename = path.basename(req.url);
-		var extension = path.extname(filename);
-		console.log("The file " + filename + " was requested.");
-		next();
-	});
+// adding functionality to log the requests
+app.use(function (req, res, next) {
+	var filename = path.basename(req.url);
+	var extension = path.extname(filename);
+	console.log("The file " + filename + " was requested.");
+	next();
+});
 	
 
-	// add an http server to serve files to the Edge browser 
-	// due to certificate issues it rejects the https files if they are not
-	// directly called in a typed URL
-	var http = require('http');
-	var httpServer = http.createServer(app); 
-	httpServer.listen(4480);
+//Add a http server to serve files to the Edge browser 
+//Aue to certificate issues it rejects the https files if they are not directly called in a typed URL
+var http = require('http');
+var httpServer = http.createServer(app); 
+httpServer.listen(4480);
 
-	app.get('/',function (req,res) {
-		res.send("hello world from the HTTP server");
-	});
+app.get('/',function (req,res) {
+	res.send("hello world from the HTTP server");
+});
 
-
-//ADDITIONAL - required for the fs to work
+//This is needed for the connection to the database to work
 var fs = require('fs');
 var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
-// now convert the configruation file into the correct format -i.e. a name/value pair array
+
+//Now convert the configruation file into the correct format -i.e. a name/value pair array
 var configarray = configtext.split(",");
 var config = {};
 for (var i = 0; i < configarray.length; i++) {
-    var split = configarray[i].split(':');
-    config[split[0].trim()] = split[1].trim();
+  var split = configarray[i].split(':');
+  config[split[0].trim()] = split[1].trim();
 }
 
 var pg = require('pg'); 
 var pool = new pg.Pool(config); 
 console.log(config);
 
-
+//This function gets the questions from the database
 app.get('/getQuestions', function (req,res) {
-     pool.connect(function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       } 
-        // use the inbuilt geoJSON functionality
-        // and create the required geoJSON format using a query adapted from here:  http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
-        // note that query needs to be a single string with no line breaks so built it up bit by bit
+  pool.connect(function(err,client,done) {
+    if(err){
+        console.log("not able to get connection "+ err);
+        res.status(400).send(err);
+    } 
+    
+    // use the inbuilt geoJSON functionality
+    // and create the required geoJSON format using a query adapted from here:  http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+    // note that query needs to be a single string with no line breaks so built it up bit by bit
 
-        	var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
-        	querystring = querystring + "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg.coordinates)::json As geometry, ";
-        	querystring = querystring + "row_to_json((SELECT l FROM (SELECT point_name, question, answer1, answer2, answer3, answer4, correct_answer) As l      )) As properties";
-        	querystring = querystring + "   FROM quizquestions  As lg limit 100  ) As f ";
-        	console.log(querystring);
-        	client.query(querystring,function(err,result){
-
-          //call `done()` to release the client back to the pool
-           done(); 
-           if(err){
-               console.log(err);
-               res.status(400).send(err);
-           }
-           res.status(200).send(result.rows);
-       });
+    var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+    querystring = querystring + "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg.coordinates)::json As geometry, ";
+    querystring = querystring + "row_to_json((SELECT l FROM (SELECT point_name, question, answer1, answer2, answer3, answer4, correct_answer) As l      )) As properties";
+    querystring = querystring + "   FROM quizquestions  As lg limit 100  ) As f ";
+    console.log(querystring);
+    client.query(querystring,function(err,result){
+      //call `done()` to release the client back to the pool
+      done(); 
+      
+      if(err){
+        console.log(err);
+        res.status(400).send(err);
+      }
+      
+      res.status(200).send(result.rows);
+      });
     });
 });
 
 app.post('/uploadAnswerData',function(req,res){
-       // note that we are using POST here as we are uploading data
-       // so the parameters form part of the BODY of the request rather than the RESTful API
-       console.dir(req.body);
-       pool.connect(function(err,client,done) {
-             if(err){
-             console.log("not able to get connection "+ err);
-             res.status(400).send(err);
-             }
+  // note that we are using POST here as we are uploading data
+  // so the parameters form part of the BODY of the request rather than the RESTful API
+console.dir(req.body);
+pool.connect(function(err,client,done) {
+  if(err){
+    console.log("not able to get connection "+ err);
+    res.status(400).send(err);
+  }
 
-             var querystring = "INSERT into quizanswers (question,answer,answer_value,answer_correct) values ('";
-             querystring = querystring + req.body.question + "','" + req.body.answer+"','" + req.body.answer_value+"','" + req.body.answer_correct+"')";
-             console.log(querystring);
-             client.query( querystring,function(err,result) {
-          done();
-          if(err){
-               console.log(err);
-               res.status(400).send(err);
-          }
-          res.status(200).send("Answer sent to database");
-       });
-}); });
+  //Creates a SQL string for the SQL statement to insert the answer into the database
+  var querystring = "INSERT into quizanswers (question,answer,answer_value,answer_correct) values ('";
+  querystring = querystring + req.body.question + "','" + req.body.answer+"','" + req.body.answer_value+"','" + req.body.answer_correct+"')";
+  console.log(querystring);
+  client.query( querystring,function(err,result) {
+    done();
+    if(err){
+      console.log(err);
+      res.status(400).send(err);
+    }
+      
+    res.status(200).send("Answer sent to database");
+    });
+  }); 
+});
 
 	
 // the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
-  app.get('/:name1', function (req, res) {
-  // run some server-side code
-  // the console is the command line of your server - you will see the console.log values in the terminal window
-  console.log('request '+req.params.name1);
+app.get('/:name1', function (req, res) {
+// run some server-side code
+// the console is the command line of your server - you will see the console.log values in the terminal window
+console.log('request '+req.params.name1);
 
-  // the res is the response that the server sends back to the browser - you will see this text in your browser window
-  res.sendFile(__dirname + '/'+req.params.name1);
+// the res is the response that the server sends back to the browser - you will see this text in your browser window
+res.sendFile(__dirname + '/'+req.params.name1);
 });
 
 
-  // the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
-  app.get('/:name1/:name2', function (req, res) {
-  // run some server-side code
-  // the console is the command line of your server - you will see the console.log values in the terminal window
-  console.log('request '+req.params.name1+"/"+req.params.name2);
+// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
+app.get('/:name1/:name2', function (req, res) {
+// run some server-side code
+// the console is the command line of your server - you will see the console.log values in the terminal window
+console.log('request '+req.params.name1+"/"+req.params.name2);
 
-  // the res is the response that the server sends back to the browser - you will see this text in your browser window
-  res.sendFile(__dirname + '/'+req.params.name1+'/'+req.params.name2);
+// the res is the response that the server sends back to the browser - you will see this text in your browser window
+res.sendFile(__dirname + '/'+req.params.name1+'/'+req.params.name2);
 });
 
 
-	// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx/xxxx
-	app.get('/:name1/:name2/:name3', function (req, res) {
-		// run some server-side code
-		// the console is the command line of your server - you will see the console.log values in the terminal window
-		console.log('request '+req.params.name1+"/"+req.params.name2+"/"+req.params.name3); 
-		// send the response
-		res.sendFile(__dirname + '/'+req.params.name1+'/'+req.params.name2+ '/'+req.params.name3);
+// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx/xxxx
+app.get('/:name1/:name2/:name3', function (req, res) {
+// run some server-side code
+// the console is the command line of your server - you will see the console.log values in the terminal window
+console.log('request '+req.params.name1+"/"+req.params.name2+"/"+req.params.name3); 
+// send the response
+res.sendFile(__dirname + '/'+req.params.name1+'/'+req.params.name2+ '/'+req.params.name3);
 	});
-  // the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx/xxxx
-  app.get('/:name1/:name2/:name3/:name4', function (req, res) {
-  // run some server-side code
-  // the console is the command line of your server - you will see the console.log values in the terminal window
- console.log('request '+req.params.name1+"/"+req.params.name2+"/"+req.params.name3+"/"+req.params.name4); 
-  // send the response
-  res.sendFile(__dirname + '/'+req.params.name1+'/'+req.params.name2+ '/'+req.params.name3+"/"+req.params.name4);
+
+// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx/xxxx
+app.get('/:name1/:name2/:name3/:name4', function (req, res) {
+// run some server-side code
+// the console is the command line of your server - you will see the console.log values in the terminal window
+console.log('request '+req.params.name1+"/"+req.params.name2+"/"+req.params.name3+"/"+req.params.name4); 
+// send the response
+res.sendFile(__dirname + '/'+req.params.name1+'/'+req.params.name2+ '/'+req.params.name3+"/"+req.params.name4);
 });
-
-
